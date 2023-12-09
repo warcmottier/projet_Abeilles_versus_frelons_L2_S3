3009e5f5c625354c8ab9831595ecd93da587be4a
Solgul <119955001+Solgul@users.noreply.github.com>
2023-12-09 15:48:54 +0100

    Update liste.c
    alloueCellule et insereEnTete

--- src/liste.c
+++ src/liste.c
@@ -1,13 +1,44 @@
 #include "liste.h"
 
-Unite* alloueCellule(char camp, char type, int force){
+Unite* alloueCellule(char camp, char type){
   Unite* ruche = malloc(sizeof(Unite));
+  if (!ruche) {
+    return NULL;
+  }
+  int force;
+  switch(type){
+    case 'r':
+      force = FREINE;
+      break;
+    case 'o':
+      force = FOUVRIERE;
+      break;
+    case 'e':
+      force = FESCADRON;
+      break;
+    case 'g':
+      force = FGUERRIERE;
+      break;
+    case 'f':
+      force = FFRELON;
+      break;
+    default :
+      force = 0;
+      break;
+  }
   (*ruche).camp = camp; (*ruche).type = type; (*roche).force = force;
   return ruche;
 }
 
 int insereEnTete(UListe* l, char type){
-  
+  Unite* tete = alloueCellule((**l).camp,type);
+  if(!tete){
+    return 0;
+  }
+  (**tete).uprec = (**l).uprec; (**tete).colprec = (**l).colprec;
+  (**l).uprec = *tete; (**l).colprec = *tete;
+  (**tete).usuiv = *l; (**tete).colsuiv = *l;
+  return 1;
 }
 
 int suprimeCellule(UListe* l, char type, int posX, int posY){
