4ee40952826edb43012857dd5e3617a9acdd197c
Solgul <119955001+Solgul@users.noreply.github.com>
2023-12-09 14:59:59 +0100

    Update liste.c

--- src/liste.c
+++ src/liste.c
@@ -1,1 +1,19 @@
 #include "liste.h"
+
+Unite* alloueCellule(char camp, char type, int force){
+  Unite* ruche = malloc(sizeof(Unite));
+  (*ruche).camp = camp; (*ruche).type = type; (*roche).force = force;
+  return ruche;
+}
+
+int insereEnTete(UListe* l, char type){
+  
+}
+
+int suprimeCellule(UListe* l, char type, int posX, int posY){
+  
+}
+
+void libereListe(UListe* l){
+  
+}
